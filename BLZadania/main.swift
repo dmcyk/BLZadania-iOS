ad8e7032f7322d0e85e350d67f6dd1180de6ba33
osjup <malarczyk.damian@hotmail.com>
2015-12-11 00:06:59 +0100

    Initial Commit

---
+++ BLZadania/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BLZadania
+//
+//  Created by Damian Malarczyk on 11.12.2015.
+//  Copyright © 2015 Damian Malarczyk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
